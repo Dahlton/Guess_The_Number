bf945e4d232b847bb6896449aafd3d018b8b12ec
Dahlton <Dahltonstack@icloud.com>
2014-03-18 18:05:08 +0000

    checked guess and secret number and returned a message

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,11 @@
-puts "Welcome to Guess The Number"
-secret_number = rand(1001)
-puts secret_number
+puts "Welcome to Guess The Number!"
+secret_number = rand(2)
+
+puts "Guess a number between 0 and 1000:"
+guess = gets.chomp.to_i
+
+if secret_number == guess
+  puts "CONGRATULATIONS! You got the number correct"
+else
+  puts "HAHAHA YOU FAIL! NOW GET OUT!!!!! #{secret_number} was the correct number"
+end
